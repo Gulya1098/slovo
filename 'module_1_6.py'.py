c30fd6dd2d23619ea2d6c4d34ad823646d30949d
Gulya1098 <mansurova221098@gmail.com>
2024-12-11 17:24:32 +0500

    Add files via upload

---
+++ 'module_1_6.py'.py
@@ -0,0 +1,15 @@
+my_dict = {'Misha': 1993, 'Vanya': 2003, 'Sema': 2018}
+print(my_dict)
+(my_dict['Misha'])
+print(my_dict.get('Антон'))
+print(my_dict)
+my_dict.update({'Katya': 1999, 'Tanya': 2007})
+print(my_dict)
+my_dict.pop('Tanya')
+print(my_dict)
+
+my_set = {1, 2, 3, 4, 2, 2, 3, 1,'Cat',(10,20,30)}
+print(my_set)
+print(my_set.remove(4))
+print(my_set)
+
